641157ffd16bf4091242dc525e39ba581a81fc45
shawngchristy <shawn.g.christy@gmail.com>
2016-03-02 16:46:34 +0000

    add cart edit view, supporting model methods, etc

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,10 @@
 class Cart < ActiveRecord::Base
     has_many :line_items, dependent: :destroy
-    
+    def subtotal
+        #SQL version faster
+        line_items.select("SUM(quantity * price) AS sum")[0].sum
+        
+        # Ruby version  slower
+        # line_items.to_a.sum{ |item| item.total }
+    end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,10 @@ class LineItem < ActiveRecord::Base
   belongs_to :cart
   #TODO: before save, reduce product inventory quantity
   #TODO: before delete/ destroy, add back to the product inventory quantity
+
+
+def total
+  price * quantity
+end
+
 end
